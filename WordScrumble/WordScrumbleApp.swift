b9608cc6fed6538eb7007a99687476f115f8091a
Thaddäus Schima <t.schima@mailbox.org>
2023-02-18 20:11:42 +0100

    Initial Commit

---
+++ WordScrumble/WordScrumbleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrumbleApp.swift
+//  WordScrumble
+//
+//  Created by Thaddäus Schima on 18.02.23.
+//
+
+import SwiftUI
+
+@main
+struct WordScrumbleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
